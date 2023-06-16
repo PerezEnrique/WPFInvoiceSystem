19474f47ad048da65132c144fdebe46eff87b67e
Jesus Enrique Perez <28enriqueperez@gmail.com>
2023-06-16 12:00:20 -0400

    Set domain Entities and enable nullability

---
+++ WPFInvoiceSystem/Domain/Entities/Invoice.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFInvoiceSystem.Domain.Entities
+{
+    public enum PaymentStatus
+    {
+        YetToPay,
+        PartiallyPaid,
+        Paid
+    }
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public virtual Customer Customer { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Exempt { get; set; }
+        public int InvoiceNumber { get; set; }
+        public virtual ObservableCollection<InvoiceService> Services { get; set; }
+        public virtual ObservableCollection<Payment> Payments { get; set; }
+        public PaymentStatus PaymentStatus
+        {
+            get
+            {
+                return Payments.Aggregate(
+                    0m,
+                    (acc, current) =>
+                    {
+                        if (current.IsChange) return acc - current.Value;
+                        return acc + current.Value;
+                    },
+                    (value) =>
+                    {
+                        if (value >= Total) return PaymentStatus.Paid;
+                        if (value > 0 && value < Total) return PaymentStatus.PartiallyPaid;
+                        return PaymentStatus.YetToPay;
+                    }
+                );
+            }
+        }
+
+        public decimal Tax { get; set; }
+        public decimal TaxBase { get; set; }
+        public decimal Total { get; set; }
+
+        public Invoice()
+        {
+            Services = new ObservableCollection<InvoiceService>();
+            Payments = new ObservableCollection<Payment>();
+        }
+    }
+}
---
+++ WPFInvoiceSystem/Domain/Entities/InvoiceService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace WPFInvoiceSystem.Domain.Entities
+{
+    public class InvoiceService
+    {
+        public int InvoiceId { get; set; }
+        public virtual Invoice Invoice { get; set; }
+        public virtual Service Service { get; set; }
+        public int ServiceId { get; set; }
+        public decimal FinalPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
