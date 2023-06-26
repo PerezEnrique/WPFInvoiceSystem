9c08fbebbfde35e33a07329c3f2c0d275bc818c3
Jesus Enrique Perez <28enriqueperez@gmail.com>
2023-06-26 11:45:18 -0400

    Implement app settings and config service

---
+++ WPFInvoiceSystem/Services/ConfigurationService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFInvoiceSystem.Services
+{
+    public static class ConfigurationService
+    {
+        private static IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        public static decimal StandardTaxRate { get; } = config.GetValue<decimal>("StandardTaxRate");
+        public static decimal GFTTRate { get; } = config.GetValue<decimal>("GFTTRate");
+    }
+}
