4b2f598fd417b719bf255f35a224ff52e8631d7c
Jesus Enrique Perez <28enriqueperez@gmail.com>
2023-06-26 11:19:46 -0400

    Implement unit of work interface

---
+++ WPFInvoiceSystem/Domain/IUnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFInvoiceSystem.Domain.Repositories;
+
+namespace WPFInvoiceSystem.Domain
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IBanksRepository BanksRepository { get; }
+        ICustomersRepository CustomersRepository { get; }
+        IInvoicesRepository InvoicesRepository { get; }
+        IPaymentsRepository PaymentsRepository { get; }
+        IPaymentMethodsRepository PaymentMethodsRepository { get; }
+        IServicesRepository ServicesRepository { get; }
+        IServiceTypesRepository ServiceTypesRepository { get; }
+        Task CompleteAsync();
+        IBaseRepository<ItemsType> GetRepository<ItemsType>();
+    }
+}
